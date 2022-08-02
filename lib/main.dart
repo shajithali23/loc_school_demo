61bb36e214291266f39ecff613871324ce118405
shajith <shajithali.s.applogiq@gmail.com>
2022-08-02 18:19:18 +0800

    ios setup

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
     debugShowCheckedModeBanner: false,
-      title: 'School Management App',
+      title: 'Institution App',
       
       home: LoginScreen(),
     );
